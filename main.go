98bfc0ceaf5794a6ed4b5278343cc4c2fabc5463
Shakeel Mahate <shakeel.mahate@gmail.com>
2018-09-07 16:57:17 -0400

    Added newlines when y cood changes.

--- main.go
+++ main.go
@@ -57,8 +57,15 @@ func main() {
 
 		var b strings.Builder
 		for i := 1; i <= reader.NumPage(); i++ {
+			// Initialize y co-ordinate for the page
+			y := 0.0
 			for _, t := range reader.Page(i).Content().Text {
-				fmt.Fprintf(&b, "%v", t.S)
+				// Check if we are on a new line
+				if t.Y != y {
+					y = t.Y
+					b.WriteString("\n")
+				}
+				b.WriteString(t.S)
 			}
 		}
 		fmt.Fprintf(writer, "%v\n", b.String())
